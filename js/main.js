15dd524cbd4588f68cbc2c5aaa5326dde7bc375d
Karin Alejo <karin_alejo@hotmail.com>
2017-02-28 15:34:03 -0500

    subiendo funciones

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ var estatura = prompt("Ingrese estatura:");
 
 function imc(peso,estatura){
   //ponga aqui la funcion
+  var calcular;
+  calcular = peso/estatura;
+  alert("El IMC es : " + calcular);
 }
